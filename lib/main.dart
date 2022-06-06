097f172557cd507cbd3f14dd283de8a802132dce
Kyaw Thu <evanch98@gmail.com>
2022-06-06 19:24:56 +0800

    Change the name of the class from MyApp to DictionaryApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DictionaryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DictionaryApp extends StatelessWidget {
+  const DictionaryApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
